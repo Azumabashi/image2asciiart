19c2c10100ea5382d1566e0bfb733896bfcadc64
Azumabashi <>
2022-11-13 02:45:32 +0900

    define chars to be used

--- image2asciiart.nim
+++ image2asciiart.nim
@@ -1,6 +1,9 @@
 import pixie
 import sequtils
 
+const
+  chars = ['@', 'W', '#', 'R', 'E', '8', 'x', 's', 'i', ';', ',', '.', ' ']
+
 proc getGrayscalePixel(pixel: ColorRGBX): int =
   result = (pixel.r + pixel.g + pixel.b) / 3
 
