b63743db79471903699a01dfb41284e58dfbc4f8
Azumabashi <>
2022-11-13 02:59:09 +0900

    match type

--- image2asciiart.nim
+++ image2asciiart.nim
@@ -1,15 +1,16 @@
 import pixie
 import sequtils
+import math
 
 const
   chars = ['@', 'W', '#', 'R', 'E', '8', 'x', 's', 'i', ';', ',', '.', ' ']
 
 proc getGrayscalePixel(pixel: ColorRGBX): int =
-  result = (pixel.r + pixel.g + pixel.b) / 3
+  result = int(int(pixel.r + pixel.g + pixel.b) / 3)
 
 proc getChar(pixel: int): char =
-  let width = 256 / chars.len + 1  # +1 to avoid index error
-  result = chars[pixel / width]
+  let width = int(256 / chars.len) + 1  # +1 to avoid index error
+  result = chars[int(floor(pixel / width))]
 
 proc image2asciiart(filename: string): seq[string] =
   let
