9fd6b2326385d2957db0c9d032e1effcdb342314
Azumabashi <>
2022-11-13 02:44:47 +0900

    get grayscaled pixel with getGrayscalePixel

--- image2asciiart.nim
+++ image2asciiart.nim
@@ -1,8 +1,11 @@
 import pixie
+import sequtils
 
 proc getGrayscalePixel(pixel: ColorRGBX): int =
   result = (pixel.r + pixel.g + pixel.b) / 3
 
 proc image2asciiart(filename: string): seq[string] =
-  let image = readImage(filename)
+  let
+    image = readImage(filename)
+    grayscalePixel = image.data.map(getGrayscalePixel)
   
