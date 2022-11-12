c1c5d553ddb3d10a0de5da4c222916a3d8dbb74c
Azumabashi <>
2022-11-13 03:00:21 +0900

    resize image when read

--- image2asciiart.nim
+++ image2asciiart.nim
@@ -12,9 +12,9 @@ proc getChar(pixel: int): char =
   let width = int(256 / chars.len) + 1  # +1 to avoid index error
   result = chars[int(floor(pixel / width))]
 
-proc image2asciiart(filename: string): seq[string] =
+proc image2asciiart(filename: string, width: int, height: int): seq[string] =
   let
-    image = readImage(filename)
+    image = readImage(filename).resize(width, height)
     grayscalePixel = image.data.map(getGrayscalePixel)
     asciiArt = grayscalePixel.map(getChar)
   for i in 0..<asciiArt.len:
