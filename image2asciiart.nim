5d7083b2969396f81cb3c1af06034301059cdd31
Azumabashi <>
2022-11-13 02:43:05 +0900

    feat: implement proc getGrayscalePixel

--- image2asciiart.nim
+++ image2asciiart.nim
@@ -1,5 +1,8 @@
 import pixie
 
+proc getGrayscalePixel(pixel: ColorRGBX): int =
+  result = (pixel.r + pixel.g + pixel.b) / 3
+
 proc image2asciiart(filename: string): seq[string] =
   let image = readImage(filename)
   
